e730e57f3bfcb42196f2d8fbde9362209efb3671
YairShlomo <33932692+YairShlomo@users.noreply.github.com>
2020-06-03 18:03:18 +0300

    split App languageSelector to a new component

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import UserCreate from "./UserCreate";
 import LanguageContext from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
-
+import LanguageSelector from "../components/LanguageSelector";
 class App extends React.Component {
   state = { language: "english" };
 
@@ -13,17 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a language:
-          <i
-            className="flag us"
-            onClick={() => this.onLanguageChanged("english")}
-          />
-          <i
-            className="flag nl"
-            onClick={() => this.onLanguageChanged("dutch")}
-          />
-        </div>
+        <LanguageSelector onLanguageChanged={this.onLanguageChanged} />
         <ColorContext.Provider value="red">
           <LanguageContext.Provider value={this.state.language}>
             <UserCreate />
---
+++ translate/src/components/LanguageSelector.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class LanguageSelector extends React.Component {
+  render() {
+    return (
+      <div>
+        Select a language:
+        <i
+          className="flag us"
+          onClick={() => this.props.onLanguageChanged("english")}
+        />
+        <i
+          className="flag nl"
+          onClick={() => this.props.onLanguageChanged("dutch")}
+        />
+      </div>
+    );
+  }
+}
+
+export default LanguageSelector;
